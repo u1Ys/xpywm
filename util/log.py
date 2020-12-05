ee3c43aa477609b576423b21d7ccbd467c268f73
u1Ys <u1@berlb.net>
2020-12-05 23:40:05 +0900

    Format logs for linter and add on-working debug function

--- util/log.py
+++ util/log.py
@@ -2,14 +2,24 @@
 
 import sys
 
+ON_WORKING_DEBUG_FILE = '/tmp/debug'
+
+
 def log(object_, prefix, fmt, *args):
     print(str(object_), prefix,
           fmt % args if args else fmt,
           file=sys.stderr,
           flush=True)
 
+
 def debug(object_, fmt, *args):
     log(object_, '** debug: ', fmt, *args)
 
+
 def error(object_, fmt, *args):
     log(object_, '** error:', fmt, *args)
+
+
+def debug_on_working(*args):
+    with open(ON_WORKING_DEBUG_FILE, mode='a') as f:
+        print(*args, file=f)
